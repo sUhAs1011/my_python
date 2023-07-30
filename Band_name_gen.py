71703ea2adcaf5da4235ac22e8ad1718a04088cb
AyushAshok <112970378+AyushAshok@users.noreply.github.com>
2023-07-30 15:09:13 +0530

    Add files via upload

---
+++ Band_name_gen.py
@@ -0,0 +1,5 @@
+# Generating Band name using your birth city and your pet name
+print("Enter the name of your birth city and your pet.\n")
+city=str(input("Enter birth city name\n"))
+pet_name=str(input("Enter pet name\n"))
+print("Your Band's name is "+ city+ " " + pet_name)
