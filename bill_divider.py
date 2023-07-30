441433e60986d9555d26887dbf8ab3940463ea04
AyushAshok <112970378+AyushAshok@users.noreply.github.com>
2023-07-30 22:11:40 +0530

    Update bill_divider.py

--- bill_divider.py
+++ bill_divider.py
@@ -1,11 +1,6 @@
-#If the bill was $150.00, split between 5 people, with 12% tip. 
+#This code splits the bill with the number of ppl sharing it,with the tip you want to pay the waiter.....
 
-#Each person should pay (150.00 / 5) * 1.12 = 33.6
-#Format the result to 2 decimal places = 33.60
 
-#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
-
-#Write your code below this line 👇
 
 bill_t=int(input("Enter the total bill: "))
 n_ppl=int(input("Enter the total ppl: "))
