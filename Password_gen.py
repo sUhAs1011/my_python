1aca8d045fdd664a61afc5971cfdd61f629d824c
AyushAshok <112970378+AyushAshok@users.noreply.github.com>
2023-08-01 19:40:40 +0530

    Add files via upload

---
+++ Password_gen.py
@@ -0,0 +1,35 @@
+#Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+let_part=''
+for i in range(nr_letters):
+  ind=(random.random()*(len(letters)-1))
+  let_part=let_part+str(letters[int(ind)])
+num_part=''
+for j in range(nr_numbers):
+  ind1=(random.random()*(len(numbers)-1))
+  num_part=num_part+str(numbers[int(ind1)])
+sym_part=''
+for k in range(nr_symbols):
+  ind2=(random.random()*(len(symbols)-1))
+  sym_part=sym_part+str(symbols[int(ind2)])
+password=let_part+sym_part+num_part
+print(password)
+s_shuffled = ''.join(random.sample(password, len(password)))
+print(s_shuffled)
+  
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+s_shuffled = ''.join(random.sample(password, len(password)))
+print(s_shuffled)
